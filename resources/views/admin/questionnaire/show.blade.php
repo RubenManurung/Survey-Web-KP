91cac111d26a2de872b1d10f19655090a7cd5407
RubenManurung <rubenmichael199@gmail.com>
2020-08-04 16:07:12 +0700

    FIX Project

--- resources/views/admin/questionnaire/show.blade.php
+++ resources/views/admin/questionnaire/show.blade.php
@@ -112,44 +112,43 @@
 @section('footer')
   <script src="https://code.highcharts.com/highcharts.js"></script>
 
+    <script type="text/javascript">
+      Highcharts.chart('chartSurvey', {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Laporan Hasil Survey'
+      },
+      xAxis: {
+          categories: {!!json_encode($pertanyaan)!!},
+          crosshair: true
+      },
+      yAxis: {
+          min: 0,
+          title: {
+              text: 'Persentase Respon'
+          }
+      },
+      tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+              '<td style="padding:0"><b>{point.y:.1f} orang</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      series: [{
+          name: 'Jumlah Responden',
+          data: {!!json_encode($responden)!!}
 
-  <script type="text/javascript">
-    Highcharts.chart('chartSurvey', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Laporan Hasil Survey'
-    },
-    xAxis: {
-        categories: {!!json_encode($pertanyaan)!!},
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Persentase Respon'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} orang</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Jumlah Responden',
-        data: {!!json_encode($responden)!!}
-
-    }]
-  });
-  </script>
+      }]
+    });
+    </script>
 @stop
